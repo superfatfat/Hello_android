d1521165ecad151af38f21ed5a4a7badc93e143c
Andy <andy61132000@yahoo.com.tw>
2015-06-24 15:34:04 +0800

    commit test 1

--- app/src/main/java/andy/div/hello_android/MainActivity.java
+++ app/src/main/java/andy/div/hello_android/MainActivity.java
@@ -7,11 +7,11 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //just for fun!!
     }
 
     @Override
